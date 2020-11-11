8581583da2db1e813e5f28c74cff0efb6e1f5b15
redwrasse <redwrasse@users.noreply.github.com>
2020-11-10 21:27:48 -0700

    working naive bayes

--- naivebayes.js
+++ naivebayes.js
@@ -44,13 +44,67 @@ function naiveBayes() {
 
 // Compute Naive Bayes for given query and documents
 function computeNaiveBayes() {
-    // todo("implement")
-    let results = {
-        "docA": Math.random(),
-        "docB": Math.random(),
-        "docC": Math.random(),
-    };
-    return results;
+
+    let query = document.getElementById("query").value;
+    let queryWords = query
+        .toLowerCase()
+        .replace(/[.]/g, '')
+        .replace(/[^a-zA-Z ]/g, "")
+        .split(/\s/);
+
+    let docA = document.getElementById("docA").value;
+    let docB = document.getElementById("docB").value;
+    let docC = document.getElementById("docC").value;
+
+    let wcA = wordCounts(docA);
+    let wcB = wordCounts(docB);
+    let wcC = wordCounts(docC);
+
+    let docs = ["docA", "docB", "docC"];
+    let allWc = [wcA, wcB, wcC];
+    let docProbs = {"docA": 1.0, "docB": 1.0, "docC": 1.0};
+    for (var i = 0; i < queryWords.length; i++) {
+        let qword = queryWords[i];
+        let qwordCounts = {"docA": 0, "docB": 0, "docC": 0};
+        for (var j = 0; j < 3; j++) {
+            if (qword in allWc[j]) {
+                qwordCounts[docs[j]] = allWc[j][qword];
+            }
+        }
+        let totalQWordCounts = sumValues(qwordCounts);
+        for (j = 0; j < 3; j++) {
+            if (totalQWordCounts === 0) {
+                docProbs[docs[j]] = 0.0;
+            } else {
+                docProbs[docs[j]] = docProbs[docs[j]] * qwordCounts[docs[j]] / totalQWordCounts;
+            }
+        }
+    }
+    let totalProbs = sumValues(docProbs);
+    for (j = 0; j < 3; j++) {
+        if (totalProbs === 0.0) {
+            docProbs[docs[j]] = 0.0;
+        } else {
+            docProbs[docs[j]] = docProbs[docs[j]] / totalProbs;
+        }
+    }
+    console.log(docProbs);
+    return docProbs;
+
+}
+
+function wordCounts(doc) {
+    let words = doc.replace(/[.]/g, '').replace(/[^a-zA-Z ]/g, "")
+        .split(/\s/);
+    let freqMap = {};
+    words.forEach(function(w) {
+        let wl = w.toLowerCase();
+        if (!freqMap[wl]) {
+            freqMap[wl] = 0;
+        }
+        freqMap[wl] += 1;
+    });
+    return freqMap;
 }
 
 
@@ -59,3 +113,12 @@ function clearChildren(node) {
         node.removeChild(node.firstChild);
 }
 
+function sumValues( obj ) {
+    var sum = 0;
+    for( var el in obj ) {
+        if( obj.hasOwnProperty( el ) ) {
+            sum += parseFloat( obj[el] );
+        }
+    }
+    return sum;
+}
